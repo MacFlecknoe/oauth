c3d49b67b843314da256e32c7123cbb2fa2558e9
Michael Lambert <mlamber7@its.jnj.com>
2014-03-12 00:16:49 -0400

    corrected url error

---
+++ oauth/oauth-lib/src/main/java/com/jnj/wp/oauth/camel/route/SecureProxy.java
@@ -0,0 +1,39 @@
+package com.jnj.wp.oauth.camel.route;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * Example route code.
+ * 
+ * @author mlamber7
+ *
+ */
+public class SecureProxy extends RouteBuilder {
+
+	private final Processor processor;
+	
+	public SecureProxy(Processor processor) {
+		this.processor = processor;
+	}
+	
+	public SecureProxy() {
+		this.processor = new TestProcessor();
+	}
+	
+	@Override
+	public void configure() throws Exception {
+		from("servlet:///message?matchOnUriPrefix=true")
+		.process(processor)
+		.setHeader(Exchange.HTTP_URI, simple("http://google.com/${header.externalUserId}?bridgeEndpoint=true&amp;throwExceptionOnFailure=false"))
+		.to("http://dummyhost");
+	}
+	
+	private static class TestProcessor implements Processor {
+
+		public void process(Exchange exchange) throws Exception {
+			exchange.getIn().setHeader("externalUserId", "voice");
+		}
+	}
+}
