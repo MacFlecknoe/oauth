67c1b62a4c3da61005d20a1a5c8adf75b95e61ac
Michael Lambert <mlamber7@its.jnj.com>
2014-03-12 14:17:39 -0400

    cleanup

--- resource/src/main/java/com/jnj/wp/resource/proxy/SecureMessageProxy.java
+++ resource/src/main/java/com/jnj/wp/resource/proxy/SecureMessageProxy.java
@@ -11,9 +11,11 @@ import org.apache.camel.builder.RouteBuilder;
  * </p>
  * <p>
  * In accordance to the Law of Demeter, the proxy class is not burdened with authentication but merely provides an endpoint to secure; 
- * it therefore needs to be wrapped in a security layer (e.g. oauth or ws-security). The proxy expects the <strong>authenticated</strong> 
- * user's id to be placed in the "externalUserId" HTTP header by the configured processor. The injected processor therefore will be tightly 
- * coupled to the authentication mechanism being used.
+ * it therefore needs to be wrapped in a security layer (e.g. oauth or ws-security).
+ * </p>
+ * <p>
+ * The proxy expects the authenticated user's id to be placed in the "externalUserId" HTTP header by the configured processor. The injected 
+ * processor therefore will be tightly coupled to the authentication mechanism being used.
  * </p>
  * 
  * @author mlamber7
